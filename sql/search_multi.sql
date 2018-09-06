1f67d60cd62291c57568f6851728555b286eb357
Pål Ellingsen <pale@unis.no>
2018-09-06 11:06:29 +0200

    Added search that handles NULL and also findes empty elements

--- sql/search_multi.sql
+++ sql/search_multi.sql
@@ -1,9 +1,23 @@
 --ACCESS=access content
 select eventdate, stationname, geartype, sampletype, eventID, parenteventID from aen
 	where eventdate between :startdate AND  :enddate AND
-	stationname LIKE :stationname AND 
-	geartype LIKE :geartype AND 
-	sampletype LIKE :sampletype AND
-       	cast(cruisenumber as text) LIKE :cruisenumber AND 
-	cast(parenteventID as text) LIKE :parenteventid
+	CASE when :stationname is not NULL THEN stationname LIKE :stationname
+    ELSE TRUE
+    END
+    AND
+    CASE when :geartype is not NULL THEN geartype LIKE :geartype 
+    ELSE TRUE
+    END
+    AND
+    CASE when :sampletype is not NULL THEN sampletype LIKE :sampletype
+    ELSE TRUE
+    END
+    AND
+    CASE when :cruisenumber is not NULL THEN cast(cruisenumber as text) LIKE :cruisenumber
+    ELSE TRUE
+    END
+    AND
+    CASE when :parenteventid is not NULL THEN cast(parenteventid as text) LIKE :parenteventid
+    ELSE TRUE
+    END
 	order by eventdate, geartype, sampletype;
