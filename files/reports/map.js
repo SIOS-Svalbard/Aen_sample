827d00caf602ebfe769fa7648eed00e4483a84ec
Pål Ellingsen <pale@unis.no>
2018-09-07 12:13:04 +0200

    Added map styleing

---
+++ files/reports/map.js
@@ -0,0 +1,51 @@
+/**
+ * @file
+ * Set behaviors related to dataTables Features.
+
+
+ (function ($) {
+  Drupal.behaviors.ForenaMap = {
+    attach: function (context, settings) {
+      //See http://www.datatables.net for documentation
+        /*var map = new OpenLayers.Map('map');
+        var wms = new OpenLayers.Layer.WMS(
+              "OpenLayers WMS",
+              "https://www.gebco.net/data_and_products/gebco_web_services/web_map_service/mapserv? ",
+              {'layers':'GEBCO_LATEST'} );
+        map.addLayer(wms);
+          var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([37.41, 8.82]),
+          zoom: 4
+        })
+      });
+    }
+  };
+*/  
+      //See http://www.datatables.net for documentation
+        /*var map = new OpenLayers.Map('map');
+        var wms = new OpenLayers.Layer.WMS(
+              "OpenLayers WMS",
+              "https://www.gebco.net/data_and_products/gebco_web_services/web_map_service/mapserv? ",
+              {'layers':'GEBCO_LATEST'} );
+        map.addLayer(wms);
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([37.41, 8.82]),
+        zoom: 4
+    })
+});*/
+
+
