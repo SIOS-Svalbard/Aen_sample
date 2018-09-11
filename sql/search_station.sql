9e8e7eda5a516ccbf209e54f2c013c9a0dd58ad8
Pål Ellingsen <pale@unis.no>
2018-09-11 17:15:04 +0200

    Updated to return a result for non defined stations

--- sql/search_station.sql
+++ sql/search_station.sql
@@ -1,4 +1,21 @@
 --ACCESS=access content
-select stationname, decimallatitude, decimallongitude from aen
-	where stationname=:stationname AND
-    sampletype = 'Station';
+Select  CASE
+	WHEN EXISTS(select stationname from aen where stationname = :stationname AND sampletype = 'Station')
+	THEN 'a defined position'
+	ELSE 'an average over its gear casts'
+	END as info,
+
+	COALESCE(
+		(select stationname from aen where stationname = :stationname AND sampletype = 'Station' ),
+		(select stationname from aen where stationname = :stationname LIMIT 1)
+		) as stationname,
+
+	COALESCE(
+		(select decimallatitude from aen where stationname = :stationname AND sampletype = 'Station'),
+		(select avg(decimallatitude) as avglat from aen where stationname = :stationname)
+		) as decimallatitude,
+
+	COALESCE(
+		(select decimallongitude from aen where stationname = :stationname AND sampletype = 'Station'),
+		(select avg(decimallongitude) as avglong from aen where stationname = :stationname)
+		) as decimallongitude;
