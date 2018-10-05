ede2b56bce82b3828b23df0e63c5a03ba43e9273
Pål Ellingsen <pale@unis.no>
2018-10-05 14:52:36 +0000

    Added record Number

--- sql/search_parentEventID.sql
+++ sql/search_parentEventID.sql
@@ -1,4 +1,4 @@
 --ACCESS=access content
-select eventdate, stationname, geartype, sampletype, eventID, parenteventID from aen
+select eventdate, stationname, geartype, sampletype, eventID, parenteventID, other -> 'recordNumber' as recordnumber from aen
 	where parenteventID=:eventid
 	order by eventdate, geartype, sampletype;
